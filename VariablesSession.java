84630929d07b731cbe21d869c46339adaf8dc28c
RutuHooli04 <xworkzrutu@gmail.com>
2023-06-20 22:49:55 +0530

    test-push

---
+++ VariablesSession.java
@@ -0,0 +1,87 @@
+public class VariableSession{
+public static void main(String[] args) {
+	int priceOfMobile = 100000;//initialiazing
+	int priceOfBook = 5000;
+	int marksOfStudent= 40;
+	int tempretureOfDay=27;
+	int numberOfPages=40;
+	int daysOfWeek=7;
+	int daysOfMonth=25;
+	int tempretureOfBody=28;
+	byte lengthOfPipes =30;
+	byte numberFemaleCandidates=26;
+	byte numberOfFiles=19;
+	String brand="butterfly";
+	String name="Ranjita";
+	String colour="pink";
+	String bird="crow";
+	String laptop="HP";
+	String watch="sonata";
+	String light="led";
+	
+	System.out.println(priceOfMobile);
+	System.out.println(priceOfBook);
+	System.out.println(marksOfStudent);
+	System.out.println(tempretureOfDay);
+	System.out.println(numberOfPages);
+	System.out.println(daysOfWeek);
+	System.out.println(daysOfMonth);
+	System.out.println(tempretureOfBody);
+	System.out.println(lengthOfPipes);
+	System.out.println(numberFemaleCandidates);
+	System.out.println(numberOfFiles);
+	System.out.println(brand);
+	System.out.println(name);
+	System.out.println(colour);
+	System.out.println(bird);
+	System.out.println(laptop);
+	System.out.println(watch);
+	System.out.println(light);
+	
+	
+	
+	
+	
+	priceOfMobile=150000; 		//reassigning values
+	priceOfBook = 200;
+	marksOfStudent= 20;
+	tempretureOfDay=17;
+	numberOfPages=400;
+	daysOfWeek=6;
+	daysOfMonth=22;
+	tempretureOfBody=26;
+	lengthOfPipes =20;
+	numberFemaleCandidates=29;
+    numberOfFiles=29;
+	brand ="preeti";
+	name="Bhagya";
+	colour="Black";
+    bird="peacock";
+	laptop="lenovo";
+    watch="titan";
+	light="led";
+	
+	System.out.println(priceOfMobile);
+	System.out.println(priceOfBook);
+	System.out.println(marksOfStudent);
+	System.out.println(tempretureOfDay);
+	System.out.println(numberOfPages);
+	System.out.println(daysOfWeek);
+	System.out.println(daysOfMonth);
+	System.out.println(tempretureOfBody);
+	System.out.println(lengthOfPipes);
+	System.out.println(numberFemaleCandidates);
+	System.out.println(numberOfFiles);
+	System.out.println(brand);
+	System.out.println(name);
+	System.out.println(colour);
+	System.out.println(bird);
+	System.out.println(laptop);
+	System.out.println(watch);
+	System.out.println(light);
+	
+	
+}
+
+}
+
