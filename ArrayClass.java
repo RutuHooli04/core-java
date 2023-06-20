d6982f75d7b90c7bfe5ae7ae0c02502c07481a88
RutuHooli04 <xworkzrutu@gmail.com>
2023-06-20 22:45:19 +0530

    test-push

---
+++ ArrayClass.java
@@ -0,0 +1,128 @@
+public class ArrayClass{
+	public static void main(String[] args){
+		int[] noOfStudents = {30,40,50,60,70};
+		char[] vowels = {'a','e','i','o','u'};
+		float[] cgpa = {7.2f,5.3f,8.9f,6.3f};
+		int[] noOfSchools = {1,2,3,4,8};
+		int[] noOfBooks = {8,9,2,5,3};
+		int[] noOfMobiles = {10000,20000,30000,4000,50000};
+		char[] names = {'r','o','h','a','n'};
+		char[] division = {'c','d','e','f','g','h'};
+		float[] tenthPercentage = {89f,63.2f,22.3f,69.3f,55.2f};
+		float [] weight = {60f,20f,10f,5f,65.3f};
+		System.out.println(noOfStudents[2]);
+		System.out.println(vowels[1]);
+		System.out.println(cgpa[3]);
+		System.out.println(noOfBooks[2]);
+		System.out.println(noOfSchools[3]);
+		System.out.println(noOfMobiles[4]);
+		System.out.println(names[2]);
+		System.out.println(division[2]);
+		System.out.println(tenthPercentage[2]);
+		System.out.println(weight[2]);
+		int[] ramOfMobiles = new int[5];
+		ramOfMobiles[0] = 8;
+		ramOfMobiles[1] = 10;
+		ramOfMobiles[2] = 20;
+		ramOfMobiles[3] = 30;
+		ramOfMobiles[4] = 60;
+		
+		int[] noOfFloors = new int[5];
+		noOfFloors[0] = 1;
+		noOfFloors[1] = 4;
+		noOfFloors[2] = 6;
+		noOfFloors[3] = 3;
+		noOfFloors[4] = 9;
+		
+		int[] lengthOfPipe = new int[5];
+		lengthOfPipe[0] = 28;
+		lengthOfPipe[1] = 48;
+		lengthOfPipe[2] = 69;
+		lengthOfPipe[3] = 35;
+		lengthOfPipe[4] = 45;
+		
+		int[] noOfFemaleCandidates = new int[5];
+		noOfFemaleCandidates[0] = 20;
+		noOfFemaleCandidates[0] = 40;
+		noOfFemaleCandidates[0] = 90;
+		noOfFemaleCandidates[0] = 30;
+		noOfFemaleCandidates[0] = 63;
+	
+	    char [] rainbowColors = new char [7];
+		rainbowColors[0] = 'y';
+		rainbowColors[1] = 'r';
+		rainbowColors[2] = 'g';
+		rainbowColors[3] = 'b';
+		rainbowColors[4] = 'i';
+		rainbowColors[5] = 'o';
+		rainbowColors[6] = 'v';
+		
+		char[] subjects = new char[5];
+		subjects[0] = 'm';
+		subjects[1] = 'k';
+		subjects[2] = 'e';
+		subjects[3] = 'h';
+		subjects[4] = 's';
+		
+		float[] age = new float[5];
+		age [0] = 10.5f;
+		age [1] = 20.5f;
+		age [2] = 90.3f;
+		age [3] = 45.2f;
+		age [4] = 30.2f;
+		
+		float[] temperature = new  float[5];
+		temperature [0] = 11.2f;
+		temperature [1] = 25.6f;
+		temperature [2] = 69.6f;
+		temperature [3] = 45.2f;
+		temperature [4] = 23.2f;
+		
+		float[] fruitsWeight = new float[5];
+		fruitsWeight[0] = 22.2f;
+		fruitsWeight[1] = 32.4f;
+		fruitsWeight[2] = 96.3f;
+		fruitsWeight[3] = 69.8f;
+		fruitsWeight[4] = 52.2f;
+		
+		char[] colorOfTree = new char[5];
+		colorOfTree[0] = 'g';
+		colorOfTree[1] = 'r';
+		colorOfTree[2] = 'e';
+		colorOfTree[3] = 'e';
+		colorOfTree[4] = 'n';
+		
+		for(int i=0;i<noOfFloors.length;i++){
+			System.out.println(noOfFloors[i]);
+		}
+		for(int i=0;i<ramOfMobiles.length;i++){
+			System.out.println(ramOfMobiles[i]);
+		}
+		for(int i=0;i<lengthOfPipe.length;i++){
+		System.out.println(lengthOfPipe[i]);
+		}
+		for(int i=0;i<noOfFemaleCandidates.length;i++){
+		System.out.println(noOfFemaleCandidates[i]);
+		}
+		for(int i=0;i<rainbowColors.length;i++){
+		System.out.println(rainbowColors[i]);
+		}
+		System.out.println(subjects);
+		System.out.println(age);
+		System.out.println(temperature);
+		System.out.println(fruitsWeight);
+		System.out.println(colorOfTree);
+		
+		
+		
+		
+		
+		
+		
+
+
+
+		
+	}
+		
+}
